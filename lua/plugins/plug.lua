6b90e340c7c7648e87e1d26d1bf33fd97a317199
Jackson Pegg <jacksonpegg01@outlook.com>
2023-08-19 11:17:49 -0400

    Ready, Set, Go!

---
+++ lua/plugins/plug.lua
@@ -0,0 +1,16 @@
+return {
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+		},
+		config = function()
+			require("neo-tree").setup()
+		end,
+	},
+	{
+		"catppuccin/nvim", name = "catppuccin",
+	},
+}
